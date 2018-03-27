09669ce9093b1d8ab572ad0183566cde2b4075c6
tgiani <s1792848@staffmail.ed.ac.uk>
2018-03-27 22:54:29 +0100

    cleaning

--- rbm_analysis.py
+++ rbm_analysis.py
@@ -30,8 +30,6 @@ from rbm_pytorch import log_diff_exp
 
 
 
-
-
 def get_ising_variables(field, sign=-1):
     """ Get the Ising variables {-1,1} representation
     of the RBM Markov fields
@@ -54,22 +52,16 @@ def ising_magnetization(field):
     return np.array([m, m * m])
 
 
-
 def energy(field):
     N = parameters['ising']['size']
     state = np.array(field).reshape((N, N))
-
     E = np.sum((-state*np.roll(state, -1, axis=0) - state*np.roll(state, -1, axis=1)))/float(N*N)
     #E = 0
     #for i in range (0,N):
     #  for j in range (0,N):
     #    E += -field[N*i+j]*field[N*i+(j+1)%N] - field[N*i+j]*field[N*(i+1)%N+j]
     #  E=E/float(N*N)
-
- 
     return np.asarray([E, E*E])
-
-
 
 
 def energy_concurrent_sampling(field, beta=1.0):
@@ -77,42 +69,32 @@ def energy_concurrent_sampling(field, beta=1.0):
     return(beta*np.apply_along_axis(energy, 1, field).transpose())
 
 
-
-
-
-
 def ising_averages(mag_history, en_history, model_size, label=""):
-
-    
- 
     resample_size = parameters['steps']-parameters['thermalization']      # number of states in each resampled set s_i
-    
-    
     # using concurrent sampling
     # magnetization
     mag_matrix = mag_history[:, 0, :]        # get a matrix with just the magnetization, along the columns we have mag of different gibbs sampled states, along the lines differen conc samplings
     mag_gibbs_avg = mag_matrix.mean(axis=0)  # take the mean across gibbs sampled states
     mag = mag_gibbs_avg.mean()               # take the mean across concurrent sampled states
     mag_error = mag_gibbs_avg.std()          # take std across concurrent sampled states
+
     # susceptibility
     susc_gibbs_avg = model_size*(mag_history[:, 1, :].mean(axis=0) - mag_gibbs_avg*mag_gibbs_avg)/parameters['temperature']
     susc = susc_gibbs_avg.mean()             # take mean cross concurrent samplings
     susc_error = susc_gibbs_avg.std()        # take std across concurrent sampled states
-
 
     # energy
     en_matrix = en_history[:, 0, :]
     en_gibbs_avg = en_matrix.mean(axis=0)
     en = en_gibbs_avg.mean()
     en_error = en_gibbs_avg.std()
+
     # heat capacity
     cv_gibbs_avg = model_size*(en_history[:, 1, :].mean(axis=0) - en_gibbs_avg*en_gibbs_avg)/(parameters['temperature']*parameters['temperature'])
     cv = cv_gibbs_avg.mean()
     cv_error = cv_gibbs_avg.std()
     
     return mag, mag_error, en, en_error, susc, susc_error, cv, cv_error
-
-
 
 
 def imgshow(file_name, img):
@@ -225,18 +207,7 @@ def sample_from_rbm(steps, model, image_size, nstates=30, v_in=None):
             magh.append(ising_magnetization(get_ising_variables(h.numpy())))
             env.append(energy_concurrent_sampling(get_ising_variables(v.numpy())))
             
-            
-
     return v, np.asarray(magv), np.asarray(magh), np.asarray(env)
-
-
-
-
-
-
-
-
-
 
 
 
@@ -265,26 +236,24 @@ model_size = parameters['ising']['size'] * parameters['ising']['size']
 rbm = rbm_pytorch.RBM(n_vis=model_size, n_hid=model_size)
 
 
-
-
 if parameters['do_convergence_analysis']:
 
   ###################################################################
-  #### Convergence analysis: observables/loglikelihood vs epochs#####
+  #### Convergence analysis: observables/loglikelihood vs epochs ####
   ###################################################################
 
+  print("Convergence analysis L=" + str(parameters['ising']['size']))
   print("Loading Ising training set...")
   train_loader = torch.utils.data.DataLoader(rbm_pytorch.CSV_Ising_dataset(parameters['ising']['train_data'], size=model_size), shuffle=True,
                                                batch_size=parameters['batch_size'], drop_last=True)
 
 
-  analysis_file = open(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/data.dat", 'w')
+  analysis_file = open(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/data.dat", 'w')
   analysis_file.write("trained rbms from " + str(parameters['checkpoint'])+ "\n")
   pbar = tqdm(range(parameters['start_epoch'], parameters['final_epoch']))
 
 
   npoints  = (parameters['final_epoch']-parameters['start_epoch'])/10
-  n = 0
 
   epochs   = np.zeros(npoints)
   mag      = np.zeros(npoints)
@@ -298,23 +267,19 @@ if parameters['do_convergence_analysis']:
   log_likelihood_mean = np.zeros(npoints)
 
 
-  for epoch in pbar:
+  for n, epoch in enumerate(pbar,0):
 
     if epoch % 10 == 0:
-
 
         print("Loading saved network state from file", parameters['checkpoint'], epoch)
         rbm.load_state_dict(torch.load(parameters['checkpoint']+str(epoch)))
         free_energy_ = []
-        
+
         # compute free energy averaging in each batch. 
         for i, (data, target) in enumerate(train_loader):
             data_input = Variable(data.view(-1, model_size))
-        
             data_free_energy = rbm.free_energy_batch_mean(data_input)  # note: it does not include Z
             free_energy_.append(data_free_energy.data[0])
-     
-     
 
         v, magv, magh, env = sample_from_rbm(parameters['steps'], rbm, parameters['ising']['size'], parameters['concurrent samples'])
         mag[n], mag_err[n], en[n], en_err[n], susc[n], susc_err[n], cv[n], cv_err[n] =  ising_averages(magv, env, model_size, "v")
@@ -328,41 +293,37 @@ if parameters['do_convergence_analysis']:
         
         print(str(epoch) + "\t" +  str(mag[n]) + "\t" + str(mag_err[n])+ "\t" +  str(en[n]) + "\t" + str(en_err[n])+ "\t" +  str(susc[n]) + "\t" + str(susc_err[n])+ "\t" +  str(cv[n]) + "\t" + str(cv_err[n]) + "\t" + str(log_likelihood_mean[n]) + "\n")
 
-        
-        n += 1
-
 
   print("Plotting....")
-
   ## Observables vs number of epoch ##
   plt.figure(figsize=(15, 5))
   plt.errorbar(epochs, mag, yerr = mag_err)
   plt.title("Magnetization vs number of epochs")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/mag" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/mag" + str(parameters['temperature']) + ".png")
   plt.close()
  
   plt.figure(figsize=(15, 5))
   plt.errorbar(epochs, en, yerr = en_err)
   plt.title("Energy vs number of epochs")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/en" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/en" + str(parameters['temperature']) + ".png")
   plt.close()
 
   plt.figure(figsize=(15, 5))
   plt.errorbar(epochs, susc, yerr = susc_err)
   plt.title("Susceptibility vs number of epochs")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/susc" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/susc" + str(parameters['temperature']) + ".png")
   plt.close()
 
   plt.figure(figsize=(15, 5))
   plt.errorbar(epochs, cv, yerr = cv_err)
   plt.title("Heat capacity vs number of epochs")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/cv" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/cv" + str(parameters['temperature']) + ".png")
   plt.close()
 
   plt.figure(figsize=(15, 5))
   plt.plot(epochs, log_likelihood_mean)
   plt.title("Loglikelihood vs number of epochs")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/loglikelihood" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/loglikelihood" + str(parameters['temperature']) + ".png")
   plt.close()
 
 
@@ -371,25 +332,25 @@ if parameters['do_convergence_analysis']:
   plt.figure(figsize=(15, 5))
   plt.errorbar(log_likelihood_mean, mag, yerr = mag_err)
   plt.title("Magnetization vs log_likelihood")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/mag_ll_" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/mag_ll_" + str(parameters['temperature']) + ".png")
   plt.close()
 
   plt.figure(figsize=(15, 5))
   plt.errorbar(log_likelihood_mean, en, yerr = en_err)
   plt.title("Energy vs log_likelihood")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/en_ll_" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/en_ll_" + str(parameters['temperature']) + ".png")
   plt.close()
  
   plt.figure(figsize=(15, 5))
   plt.errorbar(log_likelihood_mean, susc, yerr = susc_err)
   plt.title("Susceptibility vs log_likelihood")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/susc_ll_" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/susc_ll_" + str(parameters['temperature']) + ".png")
   plt.close()
 
   plt.figure(figsize=(15, 5))
   plt.errorbar(log_likelihood_mean, cv, yerr = cv_err)
   plt.title("Heat capacity vs log_likelihood")
-  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "/cv_ll_" + str(parameters['temperature']) + ".png")
+  plt.savefig(parameters['output_dir'] + "analysis_" + str(parameters['temperature']) + "_L " + str(parameters['ising']['size']) + "/cv_ll_" + str(parameters['temperature']) + ".png")
   plt.close()
 
   analysis_file.close()
@@ -400,7 +361,7 @@ else:
   ##### Temperature analysis: observables vs temperature ####
   ########################################################### 
 
-
+  print("Analysis as function of temperature")
   analysis_file = open(parameters['output_dir'] + "analysis_T_" + str(parameters['ising']['size']) + "/data_T.dat", 'w')
   analysis_file.write("trained rbms from " + str(parameters['checkpoint'])+ "\n")
 
@@ -417,17 +378,11 @@ else:
   cv_err   = np.zeros(npoints)
 
 
-
-
-
-
   for T in range(npoints):
         
         print("Loading saved network state from file", parameters['checkpoint'], T)
         rbm.load_state_dict(torch.load(parameters['checkpoint']+str(T)))
-        
-        
-     
+       
         v, magv, magh, env = sample_from_rbm(parameters['steps'], rbm, parameters['ising']['size'], parameters['concurrent samples'])
         mag[T], mag_err[T], en[T], en_err[T], susc[T], susc_err[T], cv[T], cv_err[T] =  ising_averages(magv, env, model_size, "v")
         temperatures[T] = parameters['start_temperature'] + float(T)/10 
@@ -438,19 +393,14 @@ else:
         cv = cv *(parameters['temperature']*parameters['temperature'])/(temperatures[T]*temperatures[T])
         cv_err = cv_err*(parameters['temperature']*parameters['temperature'])/(temperatures[T]*temperatures[T])
 
-        
         analysis_file.write(str(temperatures[T]) + "\t" +  str(mag[T]) + "\t" + str(mag_err[T])+ "\t" +  str(en[T]) + "\t" + str(en_err[T])+ "\t" +  str(susc[T]) + "\t" + str(susc_err[T])+ "\t" +  str(cv[T]) + "\t" + str(cv_err[T]) + "\n")
         
         print(str(temperatures[T]) + "\t" +  str(mag[T]) + "\t" + str(mag_err[T])+ "\t" +  str(en[T]) + "\t" + str(en_err[T])+ "\t" +  str(susc[T]) + "\t" + str(susc_err[T])+ "\t" +  str(cv[T]) + "\t" + str(cv_err[T]) + "\n")
-
-
-
-           
+     
 
   print("Plotting....")
 
   ## Observables vs temperature ##
-
   plt.errorbar(temperatures, mag, yerr = mag_err)
   plt.title("Magnetization vs tempearture")
   plt.savefig(parameters['output_dir'] + "analysis_T_" + str(parameters['ising']['size']) + "/mag_" + str(parameters['ising']['size']) + ".png")
@@ -474,7 +424,47 @@ else:
   analysis_file.close()
 
 
-####################################################################################
+
+"""
+example of input json file:
+
+{
+
+  "do_convergence_analysis": false,
+  "checkpoint": "/path/to/the/training/history/or/to/the/full/trained/rbms"
+  "model": "ising",
+  "temperature" : 3.0,        # tempearture of the convergence analysis
+  "start_epoch": 10,          # initial epoch from which analysis starts
+  "final_epoch": 1990,        # final epoch
+  "batch_size": 200,
+  "steps": 10000,             # gibbs sampling steps
+  "concurrent samples": 50,
+  "save interval" : 1000,
+  "output_states": false,
+  "hidden_layers": 8,
+  "thermalization": 500,
+  "image_dir": "./Data_Ising_test/",
+  "output_dir": "./Data_analysis/",
+  "ising": { "train_data": "/path/to/training/set", 
+             "size": 8 },
+  "initialize_with_training": false,
+  "start_temperature": 1.8,   # start temperature in temperature analysis
+  "final_temperature": 3.0    #final temperature 
+    
+
+ }
+
+usage:
+python rbm_analysis --json file.json
+
+"""
+
+
+
+
+
+
+###### [WIP]: Implementation of D_kl ######
 
 
 def ising_matrix():
@@ -487,13 +477,10 @@ def ising_matrix():
     return Variable(torch.from_numpy(a))  # added variable to have it workinh for Z
 
 
-
-
 def ising_free_energy(v, ising_matrix, beta=1.0):
     v = v.double()
     ising_matrix_mult = torch.sum(torch.mul(v,(v.mm(ising_matrix))), 1)   
     return ising_matrix_mult.mul(-beta)  # = -beta*E_ising
-
 
 
 def annealed_importance_sampling_ising(H, k = 1, betas = 10000, num_chains = 100):
